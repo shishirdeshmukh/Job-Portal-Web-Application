9aaeee3407d2a6359be790035bfb20e276eb1165
shishirdeshmukh <80303433+shishirdeshmukh@users.noreply.github.com>
2023-06-03 13:20:54 +0000

    phase3 added

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_DIRECTORY'] = 'uploads/'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 JOBS = [{
   'id': 1,
@@ -26,12 +31,46 @@ JOBS = [{
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html", jobs=JOBS)
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/upload")
+def up():
+
+  return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+  try:
+    file = request.files['file']
+    if file:
+      file.save(
+        os.path.join(app.config['UPLOAD_DIRECTORY'],
+                     secure_filename(file.filename)))
+
+    else:
+      return redirect('/upload')
+
+  except RequestEntityTooLarge:
+    return 'File is Larger than 16 MB Limit'
+    return redirect('/upload')
+  return redirect('/')
 
 
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
+
+@app.route("/ww")
+def contact_usq():
+  return render_template("ww.html")
+
+
+@app.route("/contactus")
+def contact_us():
+  return render_template("contact.html")
 
 
 if __name__ == "__main__":
