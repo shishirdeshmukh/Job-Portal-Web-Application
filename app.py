dc5e49883358f269cdf0b749201ba5c7af2bb5c5
shishirdeshmukh <80303433+shishirdeshmukh@users.noreply.github.com>
2023-06-12 10:52:11 +0000

    update 2.0

--- app.py
+++ app.py
@@ -2,36 +2,18 @@ from flask import Flask, render_template, jsonify, request, redirect
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
 import os
+from database import engine, load_jobs_from_db
+from sqlalchemy import text
 
 app = Flask(__name__)
 app.config['UPLOAD_DIRECTORY'] = 'uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-JOBS = [{
-  'id': 1,
-  'title': 'Data Analyst',
-  'location': 'Pune, India',
-  'Salary': 'Rs 1,20,000 Per Annum'
-}, {
-  'id': 2,
-  'title': 'Java Dev',
-  'location': 'Mumbai, India',
-  'Salary': 'Rs 4,00,000 Per Annum'
-}, {
-  'id': 3,
-  'title': 'Python Developer',
-  'location': 'Pune, India',
-}, {
-  'id': 4,
-  'title': 'Fluteer Developer',
-  'location': 'Remote',
-  'Salary': 'Rs 4,22,000 Per Annum'
-}]
-
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS)
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/upload")
@@ -60,7 +42,8 @@ def upload():
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
 @app.route("/ww")
