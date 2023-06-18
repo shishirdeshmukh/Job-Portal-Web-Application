0130f7fcadbcb57e5eb7ad3401cd6e2e990cdd37
shishirdeshmukh <80303433+shishirdeshmukh@users.noreply.github.com>
2023-06-18 04:56:45 +0000

    xd

--- app.py
+++ app.py
@@ -1,24 +1,126 @@
-from flask import Flask, render_template, jsonify, request, redirect
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session, flash
+import mysql.connector
+
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
 import os
-from database import engine, load_jobs_from_db
+from database import engine, load_jobs_from_db, load_job_from_db, add_application_to_db, sign_to_app
 from sqlalchemy import text
 
 app = Flask(__name__)
 app.config['UPLOAD_DIRECTORY'] = 'uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.secret_key = os.urandom(24)
+
+ssl_ca = '/etc/ssl/certs/ca-certificates.crt'  # Path to the SSL CA file
+
+conn = mysql.connector.connect(
+  host="aws.connect.psdb.cloud",
+  user="8h0kr4j5upc86moavov4",
+  password="pscale_pw_2isnZ9xdQj99xqa5weonYR2qJmEorIngES5emQu6cYi",
+  database="qwerty",
+  ssl_ca=ssl_ca,
+)
+
+cursor = conn.cursor()
+
+
+@app.route("/home")
+def hello_world():
+  if 'id' in session:
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs)
+  else:
+    return redirect('login.html')
+
+
+@app.route("/signup")
+def signup():
+  return render_template('signup.html')
 
 
 @app.route("/")
-def hello_world():
-  jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs)
+def login():
+  return render_template('login.html')
+
+
+@app.route("/login_vald", methods=['POST'])
+def login_vald():
+  email = request.form.get('email')
+  password = request.form.get('password')
+
+  cursor.execute(
+    """SELECT * from `users` WHERE `email` LIKE '{}' AND `password`  LIKE '{}'"""
+    .format(email, password))
+  users = cursor.fetchall()
+  if len(users) > 0:
+    session['id'] = users[0][0]
+    return redirect('/home')
+  else:
+    flash("User Registered Successfully")
+    return redirect('/')
+
+
+from werkzeug.security import generate_password_hash
+from flask import flash
+
+
+@app.route("/add_user", methods=['POST'])
+def add_user():
+  name = request.form.get('uname')
+  email = request.form.get('uemail')
+  password = request.form.get('upassword')
+
+  hashed_password = generate_password_hash(password)
+
+  query = """INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, %s, %s, %s)"""
+  values = (name, email, hashed_password)
+
+  cursor.execute(query, values)
+  conn.commit()
+
+  cursor.execute("""SELECT * FROM `users` WHERE `email` = %s""", (email, ))
+  myuser = cursor.fetchall()
+  session['id'] = myuser[0][0]
+
+  flash("User Registered Successfully")
+
+  return redirect('/')
+
+
+@app.route("/job/<id>")
+def showjob(id):
+  job = load_job_from_db(id)
+  if job:
+    return render_template('jobpage.html', job=job)
+  else:
+    return render_template('jobnotfound.html')
+
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
+@app.route("/logout")
+def logout():
+  session.pop('id')
+  return redirect('/')
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 @app.route("/upload")
 def up():
-
   return render_template('upload.html')
 
 
@@ -30,7 +132,6 @@ def upload():
       file.save(
         os.path.join(app.config['UPLOAD_DIRECTORY'],
                      secure_filename(file.filename)))
-
     else:
       return redirect('/upload')
 
@@ -44,11 +145,6 @@ def upload():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
-
-
-@app.route("/ww")
-def contact_usq():
-  return render_template("ww.html")
 
 
 @app.route("/contactus")
--- database.py
+++ database.py
@@ -1,45 +1,65 @@
 from sqlalchemy import create_engine, text
+import traceback
 
-db_connection_string = 'mysql+pymysql://1aabylr2peqpidlkvkiu:pscale_pw_Dt8AeuyulrgqsmdiOaXh7UhS5AvMMSi6HLz5V0FUMJt@aws.connect.psdb.cloud/qwerty?charset=utf8mb4'
+
+db_connection_string = 'mysql+pymysql://8h0kr4j5upc86moavov4:pscale_pw_2isnZ9xdQj99xqa5weonYR2qJmEorIngES5emQu6cYi@aws.connect.psdb.cloud/qwerty?charset=utf8mb4'
 
 engine = create_engine(
-    db_connection_string,
-    connect_args={"ssl": {"ssl_ca": "/etc/ssl/certs/ca-certificates.crt"}}
-)
+  db_connection_string,
+  connect_args={"ssl": {
+    "ssl_ca": "/etc/ssl/certs/ca-certificates.crt"
+  }})
 
 
 def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM jobs"))
-        jobs = []
-        for row in result.all():
-            jobs.append(dict(row._asdict()))  # Convert row to dictionary
-        return jobs
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row._asdict()))  # Convert row to dictionary
+    return jobs
 
 
 def load_job_from_db(id):
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), val=id)
-        rows = result.all()
-        if len(rows) == 0:
-            return None
-        else:
-            return dict(rows[0]._asdict())  # Convert row to dictionary
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}  # Pass `val` as a parameter dictionary
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0]._asdict())  # Convert row to dictionary
 
 
 def add_application_to_db(job_id, data):
-    with engine.connect() as conn:
-        query = text(
-            "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
-        )
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+    try:
+      conn.execute(query,
+                   job_id=job_id,
+                   full_name=data['full_name'],
+                   email=data['email'],
+                   linkedin_url=data['linkedin_url'],
+                   education=data['education'],
+                   work_experience=data['work_experience'],
+                   resume_url=data['resume_url'])
+    except Exception as e:
+      print(f"Error: {e}")
+      traceback.print_exc()
 
-        conn.execute(
-            query,
-            job_id=job_id,
-            full_name=data['full_name'],
-            email=data['email'],
-            linkedin_url=data['linkedin_url'],
-            education=data['education'],
-            work_experience=data['work_experience'],
-            resume_url=data['resume_url']
-        )
+
+def sign_to_app(email, name, password):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO Userdata (name, email, password) VALUES (:name, :email, :password)"
+    )
+    try:
+      conn.execute(query, name=name, email=email, password=password)
+      return True
+    except Exception as e:
+      print(f"Error: {e}")
+      return False
