0160beb22f5ab0ae6cef386de566d4029bb01b3f
shishirdeshmukh <80303433+shishirdeshmukh@users.noreply.github.com>
2023-06-18 16:09:20 +0000

    update2

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for, session, flash
 import mysql.connector
-
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
 import os
 from database import engine, load_jobs_from_db, load_job_from_db, add_application_to_db, sign_to_app
 from sqlalchemy import text
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config['UPLOAD_DIRECTORY'] = 'uploads/'
@@ -16,8 +16,8 @@ ssl_ca = '/etc/ssl/certs/ca-certificates.crt'  # Path to the SSL CA file
 
 conn = mysql.connector.connect(
   host="aws.connect.psdb.cloud",
-  user="8h0kr4j5upc86moavov4",
-  password="pscale_pw_2isnZ9xdQj99xqa5weonYR2qJmEorIngES5emQu6cYi",
+  user="eutsqk0p3s6jjosddwtl",
+  password="pscale_pw_6j1bmyBSLOqTfRDGLpPX4LBWxJFAFEIsewySYw4c3cy",
   database="qwerty",
   ssl_ca=ssl_ca,
 )
@@ -27,11 +27,14 @@ cursor = conn.cursor()
 
 @app.route("/home")
 def hello_world():
-  if 'id' in session:
-    jobs = load_jobs_from_db()
-    return render_template('home.html', jobs=jobs)
-  else:
-    return redirect('login.html')
+  try:
+    if 'id' in session:
+      jobs = load_jobs_from_db()
+      return render_template('home.html', jobs=jobs)
+    else:
+      return redirect('login.html')
+  except Exception as e:
+    return render_template('error.html', error=str(e))
 
 
 @app.route("/signup")
@@ -46,61 +49,69 @@ def login():
 
 @app.route("/login_vald", methods=['POST'])
 def login_vald():
-  email = request.form.get('email')
-  password = request.form.get('password')
+  try:
+    email = request.form.get('email')
+    password = request.form.get('password')
 
-  cursor.execute(
-    """SELECT * from `users` WHERE `email` LIKE '{}' AND `password`  LIKE '{}'"""
-    .format(email, password))
-  users = cursor.fetchall()
-  if len(users) > 0:
-    session['id'] = users[0][0]
-    return redirect('/home')
-  else:
-    flash("User Registered Successfully")
-    return redirect('/')
-
-
-from werkzeug.security import generate_password_hash
-from flask import flash
+    cursor.execute(
+      """SELECT * from `users` WHERE `email` LIKE '{}' AND `password`  LIKE '{}'"""
+      .format(email, password))
+    users = cursor.fetchall()
+    if len(users) > 0:
+      session['id'] = users[0][0]
+      return redirect('/home')
+    else:
+      flash("Wrong Details")
+      return redirect('/')
+  except Exception as e:
+    return render_template('error.html', error=str(e))
 
 
 @app.route("/add_user", methods=['POST'])
 def add_user():
-  name = request.form.get('uname')
-  email = request.form.get('uemail')
-  password = request.form.get('upassword')
+  try:
+    name = request.form.get('uname')
+    email = request.form.get('uemail')
+    password = request.form.get('upassword')
 
-  hashed_password = generate_password_hash(password)
+    hashed_password = generate_password_hash(password)
 
-  query = """INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, %s, %s, %s)"""
-  values = (name, email, hashed_password)
+    query = """INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, %s, %s, %s)"""
+    values = (name, email, hashed_password)
 
-  cursor.execute(query, values)
-  conn.commit()
+    cursor.execute(query, values)
+    conn.commit()
 
-  cursor.execute("""SELECT * FROM `users` WHERE `email` = %s""", (email, ))
-  myuser = cursor.fetchall()
-  session['id'] = myuser[0][0]
+    cursor.execute("""SELECT * FROM `users` WHERE `email` = %s""", (email, ))
+    myuser = cursor.fetchall()
+    session['id'] = myuser[0][0]
 
-  flash("User Registered Successfully")
+    flash("User Registered Successfully")
 
-  return redirect('/')
+    return redirect('/')
+  except Exception as e:
+    return render_template('error.html', error=str(e))
 
 
 @app.route("/job/<id>")
 def showjob(id):
-  job = load_job_from_db(id)
-  if job:
-    return render_template('jobpage.html', job=job)
-  else:
-    return render_template('jobnotfound.html')
+  try:
+    job = load_job_from_db(id)
+    if job:
+      return render_template('jobpage.html', job=job)
+    else:
+      return render_template('jobnotfound.html')
+  except Exception as e:
+    return render_template('error.html', error=str(e))
 
 
 @app.route("/api/job/<id>")
 def show_job_json(id):
-  job = load_job_from_db(id)
-  return jsonify(job)
+  try:
+    job = load_job_from_db(id)
+    return jsonify(job)
+  except Exception as e:
+    return jsonify({'error': str(e)})
 
 
 @app.route("/logout")
@@ -111,12 +122,15 @@ def logout():
 
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
-  data = request.form
-  job = load_job_from_db(id)
-  add_application_to_db(id, data)
-  return render_template('application_submitted.html',
-                         application=data,
-                         job=job)
+  try:
+    data = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id, data)
+    return render_template('application_submitted.html',
+                           application=data,
+                           job=job)
+  except Exception as e:
+    return render_template('error.html', error=str(e))
 
 
 @app.route("/upload")
@@ -134,17 +148,20 @@ def upload():
                      secure_filename(file.filename)))
     else:
       return redirect('/upload')
-
   except RequestEntityTooLarge:
-    return 'File is Larger than 16 MB Limit'
-    return redirect('/upload')
+    return render_template('error.html',
+                           error='File is Larger than 16 MB Limit')
+
   return redirect('/')
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs = load_jobs_from_db()
-  return jsonify(jobs)
+  try:
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
+  except Exception as e:
+    return jsonify({'error': str(e)})
 
 
 @app.route("/contactus")
--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine, text
 import traceback
 
-
-db_connection_string = 'mysql+pymysql://8h0kr4j5upc86moavov4:pscale_pw_2isnZ9xdQj99xqa5weonYR2qJmEorIngES5emQu6cYi@aws.connect.psdb.cloud/qwerty?charset=utf8mb4'
+db_connection_string = 'mysql+pymysql://eutsqk0p3s6jjosddwtl:pscale_pw_6j1bmyBSLOqTfRDGLpPX4LBWxJFAFEIsewySYw4c3cy@aws.connect.psdb.cloud/qwerty?charset=utf8mb4'
 
 engine = create_engine(
   db_connection_string,
